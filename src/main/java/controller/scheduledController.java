aff129391d8d43ad7159665a958ac6055b5b24ff
cjgithub01 <13223073681@163.cm>
2019-06-17 09:59:01 +0800

    test5

--- src/main/java/controller/scheduledController.java
+++ src/main/java/controller/scheduledController.java
@@ -15,7 +15,7 @@ public class scheduledController {
 
     @Scheduled(fixedDelay = 5000)
     public String printString(){
-        System.err.println("hello worldwqwaeqr");
+        System.err.println("hello world");
 
         return "wwww";
     }
